96b2c24d5aac01e9d7d57f1854f8905f607620de
Samiksha Mahure <76656257+samiksha-14@users.noreply.github.com>
2022-10-16 17:50:49 +0530

    Update 0053-maximum-subarray.cpp

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // Brute force : With using two for loops to interate over all subarrays and
-        // calculating their sum 
-        // TC:O(n)
-        // Optimized Soln
-        //Kadane's algorithm
-        //the  idea is to only consider a subarray until it is giving you
-        //the positive sum and keep the max count. if you encounter a subaaray
-        //giving you the neg sum just start considering the new subarray after
-        //it by initializing sum as 0
-        //tc O(n) space O(1)
         if(nums.size()==1) return nums[0];
         int sum=0,mx_sum=nums[0];
         for(int i=0;i<nums.size();i++){
